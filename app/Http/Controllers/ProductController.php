deb44f760e967b7ad4a8bee21dfefcc08bcaaf14
Jubaer Hossain <Jubaer@shuttlebd.com>
2019-02-10 18:30:46 +0600

    Product Resource Created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Product;
+use App\Http\Resources\Product\ProductResource;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        return Product::all();
+    }
+    public function show(Product $product){
+        //return $product;
+        return new ProductResource($product);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
- Route::apiResource('/product', 'ProductController');
+ Route::apiResource('/products', 'ProductController');
 
  Route::group(['prefix' => 'products'],function(){
         Route::apiResource('/{product}/reviews','ReviewController');
