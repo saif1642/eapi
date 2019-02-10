d3fbac1734ea912c2ce8bff84cb51a238f6fded7
Jubaer Hossain <Jubaer@shuttlebd.com>
2019-02-10 16:22:30 +0600

    Relationship Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
-    //
+    public function product(){
+        $this->belongsTo(Product::class);
+    }
 }
